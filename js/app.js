d145be2983affe94c1f2044c1c4ddc9a753a4174
belal011 <belalma3arek@gmail.com>
2021-08-21 22:27:03 +0200

    main files

---
+++ js/app.js
@@ -0,0 +1,135 @@
+// Set section1 as active
+document.querySelector("#section1").classList.add("active");
+
+/**
+ *
+ * Start Helper Functions
+ *
+ */
+
+function isScrolledIntoView(e) {
+  var element = e;
+  var position = element.getBoundingClientRect();
+
+  // checking for element visibility ..
+  return position.top + 20 >= 0 && position.bottom - 10 <= window.innerHeight;
+}
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ */
+
+// build the nav
+
+var holder = document.createDocumentFragment();
+var sections = document.querySelectorAll("main section");
+
+for (let i of sections) {
+  let navItem = document.createElement("li");
+  let data = i.getAttribute("data-nav").split(" ");
+  navItem.innerText = data.join(" ");
+  navItem.setAttribute("data", String(data[1]));
+  navItem.classList.add("menu__link");
+  holder.appendChild(navItem);
+}
+
+var con = document.querySelector(".page__header nav ul");
+con.appendChild(holder);
+
+// Add class 'active' to section when near top of viewport
+function add_rem_active() {
+  for (let el of document.querySelectorAll("main section ")) {
+    if (isScrolledIntoView(el)) {
+      el.classList.add("active");
+    } else {
+      el.classList.remove("active");
+    }
+  }
+}
+
+// Scroll to anchor ID using scrollIntoView
+
+function scroll_(ele) {
+  let target = document.getElementById("section" + ele.getAttribute("data"));
+  target.scrollIntoView({ block: "start", behavior: "smooth" });
+}
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+// set sections as active ..
+document.addEventListener("scroll", add_rem_active);
+
+// Scroll to section on li click
+con.addEventListener("click", function (ev) {
+  if (ev.target.nodeName === "LI") {
+    let ele =
+      document.getElementsByClassName("menu__link")[
+        parseInt(ev.target.attributes["data"].nodeValue) - 1
+      ];
+    scroll_(ele);
+  }
+});
+
+// adding top icon in the corner of the screen ..
+document.addEventListener("scroll", function () {
+  let myIcon = document.querySelector("span.icon");
+  if (window.scrollY > 250) {
+    myIcon.classList.add("un-hide");
+  } else {
+    myIcon.classList.remove("un-hide");
+  }
+});
+
+document.querySelector("span.icon").addEventListener("click", function () {
+  scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+//----------------------Additional----------------------//
+
+// var scrolling;
+
+// function hide() {
+//   if (!(window.scrollY < 250) && scrolling == false) {
+//     let header = document.querySelector(".page__header");
+//     header.classList.add("hide");
+//   } else {
+//     let header = document.querySelector(".page__header");
+//     header.classList.remove("hide");
+//   }
+// }
+
+// window.addEventListener("scroll", function () {
+//   if (
+//     !document.querySelector(".page__header").classList.contains("hide") &&
+//     window.scrollY > 250 &&
+//     window.scrollY !== 0
+//   ) {
+//     scrolling = false;
+//     let time = 4500;
+//     var time_ = setTimeout(hide, time);
+//   } else {
+//     re_assign();
+//     scrolling = true;
+//     document.querySelector(".page__header").classList.remove("hide");
+//   }
+// });
+
+// function re_assign() {
+//   var id = setInterval(force, 10);
+//   function force() {
+//     scrolling = true;
+//   }
+//   var set_t = setTimeout(function () {
+//     clearInterval(id);
+//     scrolling = false;
+//     hide();
+//   }, 3000);
+// }
